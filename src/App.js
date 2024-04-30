adecf51cb66374cc41efe284ad1f01585c913455
Zaghman Saghir <zaghmans110@gmail.com>
2024-04-30 21:14:33 +0200

    Initial changes made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 
 import './App.css';
+import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev')
+  // let role = 'dev';
+  const showEmployee = true;
+
   return (
     <div >
-      <h1> React</h1>
+
+      {
+        showEmployee ?
+          <>
+            <input type='text' onChange={(e) => {
+              console.log(e.target.value)  // To get what typed into the output
+              setRole(e.target.value);
+            }} />
+            <Employee name="John" role="Intern" />
+            <Employee name="Mark" role={role} />
+          </>
+
+          :
+          <p> You cannot see the employees</p>
+      }
     </div>
   );
 }
